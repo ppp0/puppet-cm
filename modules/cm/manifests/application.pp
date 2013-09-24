245b820871710040129a92a17a26b943316d0dc2
Tomasz Durka <tomasz@durka.pl>
2013-09-24 17:56:09 +0200

    Require cm::httpd

--- modules/cm/manifests/application.pp
+++ modules/cm/manifests/application.pp
@@ -1,6 +1,8 @@
 define cm::application ($path, $web = true, $domains = ['*'], $crons = undef) {
 
   if $web {
+    require 'cm::httpd'
+
     apache2::vhost {$name:
       content => template('cm/application/vhost.conf'),
     }
