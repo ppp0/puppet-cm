22130afea6412be41adb2daa4666bfa61ad68b60
Tomasz Durka <tomasz@durka.pl>
2013-10-23 01:32:14 +0200

    Add php5 extensions (redis, intl, memcache)

--- modules/cm/manifests/httpd.pp
+++ modules/cm/manifests/httpd.pp
@@ -4,4 +4,7 @@ class cm::httpd {
   require 'php5::apache2'
   require 'php5::extension::apc'
   require 'php5::extension::mysql'
+  require 'php5::extension::redis'
+  require 'php5::extension::intl'
+  require 'php5::extension::memcache'
 }
