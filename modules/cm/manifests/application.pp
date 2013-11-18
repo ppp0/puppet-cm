8e6c6e0de79f1f7cdd6240f86ee6ee543195af1f
Tomasz Durka <tomasz@durka.pl>
2013-11-18 17:31:52 +0100

    Remove apache dependency for now

--- modules/cm/manifests/application.pp
+++ modules/cm/manifests/application.pp
@@ -8,14 +8,4 @@ define cm::application ($path, $vhosts = {}, $crons = undef, $debug = false) {
   require 'php5::extension::intl'
   require 'php5::extension::memcache'
   require 'uglify'
-
-  if size($vhosts) {
-    require 'apache2'
-    require 'apache2::mod::rewrite'
-    require 'php5::apache2'
-
-    apache2::vhost {$name:
-      content => template('cm/application/vhost.conf'),
-    }
-  }
 }
